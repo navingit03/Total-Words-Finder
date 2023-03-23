2012e8e2bd34576abf152a6f9a7e9df22de2472c
Navin Venkatesh K <112114128+navingit03@users.noreply.github.com>
2023-03-23 10:45:42 +0530

    Project

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function convert(){
-    const str=document.getElementById("input").value;
-    str.trim();
+    let str=document.getElementById("input").value;
+    str=str.trim();
     const result=document.getElementById("result");
     if(str==="")
     {
         alert("Enter a sentence")
         return;
     }
-    const arr=str.split(" ");
+    const arr=str.split("\\s+");
     result.innerHTML="Total no of words : "+arr.length;
 }
